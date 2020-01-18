5b2f061fb6ee72540ba25b4fea86b81173e7d1fe
somaia-khalil <sumaya_khair@live.com>
2020-01-18 15:35:10 -0600

    add Atom

---
+++ Array.rb
@@ -0,0 +1,16 @@
+friends = Array["Ahmed", "Sumaya", "Hamzah", false , 2.5]
+# puts friends
+# puts friends[3]
+# puts friends[-3]
+# puts friends[1,3]
+#
+#
+# puts friends[0] = "Mohammed"
+
+# now as we knew that some of methoeds can be used with numbers and strings also
+# also there is methoeds can be used with arrays
+
+puts friends.length()
+puts friends.include? "Ahmed"
+puts friends.reverse() #reverse them
+puts friends.sort() #sort as alphabet
